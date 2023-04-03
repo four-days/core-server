3df69be58a99ce925d36bb778e6a5803225b03a2
김경석 <0xe82de@gmail.com>
2023-04-03 21:42:38 +0900

    #17 test: 원본 URL 로직 테스트 추가

--- src/test/java/com/fourdays/core/domain/url/model/service/UrlServiceImplTest.java
+++ src/test/java/com/fourdays/core/domain/url/model/service/UrlServiceImplTest.java
@@ -1,8 +1,8 @@
 package com.fourdays.core.domain.url.model.service;
 
-import com.fourdays.core.domain.url.model.repository.UrlRepository;
+import com.fourdays.core.domain.url.model.entity.URL;
 import com.fourdays.core.domain.url.model.repository.UrlRepositoryMemory;
-import org.junit.jupiter.api.BeforeEach;
+import com.fourdays.core.domain.url.util.Base62EncoderUsingNanoTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,20 +10,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class UrlServiceImplTest {
 
-    private UrlRepository urlRepository;
-
-    @BeforeEach
-    void setUp() {
-        urlRepository = new UrlRepositoryMemory();
-    }
-
     @Test
     @DisplayName("shortenUrl > urlKeyCheck")
     void shortenUrlTest_urlKeyCheck() {
-        UrlService urlService = new UrlServiceImpl(urlRepository, () -> "urlKey");
+        UrlService urlService = new UrlServiceImpl(new UrlRepositoryMemory(), () -> "urlKey");
         String originalUrl = "https://four.days/";
         String urlKey = urlService.shortenUrl(originalUrl);
 
         assertThat(urlKey).isEqualTo("urlKey");
+    }
+
+    @Test
+    @DisplayName("findOriginalUrlByUrlKey > exists")
+    void findOriginalUrlByUrlKeyTest_exists() {
+        UrlRepositoryMemory urlRepository = new UrlRepositoryMemory();
+        urlRepository.save(new URL("urlKey", "https://four.days/"));
+        UrlServiceImpl urlService = new UrlServiceImpl(urlRepository, new Base62EncoderUsingNanoTime());
+        String originalUrl = urlService.findOriginalUrlByUrlKey("urlKey");
+        assertThat(originalUrl).isEqualTo("https://four.days/");
+    }
+
+    @Test
+    @DisplayName("findOriginalUrlByUrlKey > not exists")
+    void findOriginalUrlByUrlKeyTest_notExists() {
+        UrlRepositoryMemory urlRepository = new UrlRepositoryMemory();
+        UrlServiceImpl urlService = new UrlServiceImpl(urlRepository, new Base62EncoderUsingNanoTime());
+        String originalUrl = urlService.findOriginalUrlByUrlKey("urlKey");
+        assertThat(originalUrl).isNull();
     }
 }
