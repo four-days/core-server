501f60e2a43a6efd7f135cc651b35ce2ba2a5590
김경석 <0xe82de@gmail.com>
2023-07-01 19:19:56 +0900

    #32 test: 테스트 수정

--- src/test/java/com/fourdays/core/domain/url/model/service/UrlServiceImplTest.java
+++ src/test/java/com/fourdays/core/domain/url/model/service/UrlServiceImplTest.java
@@ -1,6 +1,8 @@
 package com.fourdays.core.domain.url.model.service;
 
+import com.fourdays.core.domain.url.model.entity.Protocol;
 import com.fourdays.core.domain.url.model.entity.URL;
+import com.fourdays.core.domain.url.model.repository.ProtocolRepositoryMemory;
 import com.fourdays.core.domain.url.model.repository.UrlRepositoryMemory;
 import com.fourdays.core.domain.url.util.Base62EncoderUsingNanoTime;
 import org.junit.jupiter.api.DisplayName;
@@ -13,7 +15,7 @@ class UrlServiceImplTest {
     @Test
     @DisplayName("url 을 전달하면 url 을 단축해서 key 값을 반환한다.")
     void shortenUrlTest_keyCheck() {
-        UrlService urlService = new UrlServiceImpl(new UrlRepositoryMemory(), () -> "key");
+        UrlService urlService = new UrlServiceImpl(() -> "key", new UrlRepositoryMemory(), new ProtocolRepositoryMemory());
         String originalUrl = "https://four.days/";
         String key = urlService.shortenUrl(originalUrl);
 
@@ -24,8 +26,9 @@ class UrlServiceImplTest {
     @DisplayName("key 로 원본 url 을 찾을 수 있다.")
     void findOriginalUrlByKeyTest_exists() {
         UrlRepositoryMemory urlRepository = new UrlRepositoryMemory();
-        urlRepository.save(new URL("key", "https://four.days/"));
-        UrlServiceImpl urlService = new UrlServiceImpl(urlRepository, new Base62EncoderUsingNanoTime());
+        Protocol https = new Protocol(1, "HTTPS");
+        urlRepository.save(new URL("key", https, "https://four.days/"));
+        UrlServiceImpl urlService = new UrlServiceImpl(new Base62EncoderUsingNanoTime(), urlRepository, new ProtocolRepositoryMemory());
         String originalUrl = urlService.findOriginalUrlByKey("key");
         assertThat(originalUrl).isEqualTo("https://four.days/");
     }
@@ -34,7 +37,7 @@ class UrlServiceImplTest {
     @DisplayName("유효하지 않은 key 를 전달하면 null 이 반환된다.")
     void findOriginalUrlByKeyTest_notExists() {
         UrlRepositoryMemory urlRepository = new UrlRepositoryMemory();
-        UrlServiceImpl urlService = new UrlServiceImpl(urlRepository, new Base62EncoderUsingNanoTime());
+        UrlServiceImpl urlService = new UrlServiceImpl(new Base62EncoderUsingNanoTime(), urlRepository, new ProtocolRepositoryMemory());
         String originalUrl = urlService.findOriginalUrlByKey("key");
         assertThat(originalUrl).isNull();
     }
