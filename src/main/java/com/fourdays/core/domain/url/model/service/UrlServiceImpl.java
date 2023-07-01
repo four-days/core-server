ff7ad5b3ec4a47680aa7980d6ad46412f83d8620
김경석 <0xe82de@gmail.com>
2023-07-01 19:19:41 +0900

    #32 refactor: url 서비스 리팩토링

--- src/main/java/com/fourdays/core/domain/url/model/service/UrlServiceImpl.java
+++ src/main/java/com/fourdays/core/domain/url/model/service/UrlServiceImpl.java
@@ -1,6 +1,9 @@
 package com.fourdays.core.domain.url.model.service;
 
+import com.fourdays.core.domain.url.model.entity.Protocol;
 import com.fourdays.core.domain.url.model.entity.URL;
+import com.fourdays.core.domain.url.model.entity.exception.InvalidProtocolException;
+import com.fourdays.core.domain.url.model.repository.ProtocolRepository;
 import com.fourdays.core.domain.url.model.repository.UrlRepository;
 import com.fourdays.core.domain.url.util.Base62Encoder;
 import lombok.RequiredArgsConstructor;
@@ -10,14 +13,17 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UrlServiceImpl implements UrlService {
 
+    private final Base62Encoder base62Encoder;
+
     private final UrlRepository urlRepository;
 
-    private final Base62Encoder base62Encoder;
+    private final ProtocolRepository protocolRepository;
 
     @Override
     public String shortenUrl(String originalUrl) {
         String key = base62Encoder.encode();
-        return urlRepository.save(new URL(key, originalUrl))
+        Protocol protocol = getProtocol(originalUrl);
+        return urlRepository.save(new URL(key, protocol, originalUrl))
                 .getKey();
     }
 
@@ -27,5 +33,20 @@ public class UrlServiceImpl implements UrlService {
                 .orElse(null);
 
         return findUrl == null ? null : findUrl.getOriginal();
+    }
+
+    private Protocol getProtocol(String url) {
+        String protocolName = parseProtocol(url);
+        return protocolRepository.findByName(protocolName)
+                .orElseThrow(() -> new InvalidProtocolException("protocol does not exist. parsed protocol name=" + protocolName));
+    }
+
+    private String parseProtocol(String url) {
+        String protocolName = url.split("://")[0];
+        if ("".equals(protocolName)) {
+            throw new InvalidProtocolException("protocol name is invalid. protocol name=" + protocolName);
+        }
+
+        return protocolName;
     }
 }
