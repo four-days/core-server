caa3aa277b2f876a5b36510863247dcb0ddf918c
KyoungSeok Kim <0xe82de@gmail.com>
2023-07-04 23:30:21 +0900

    #36 MyBatis 설정 (#37)
    * #36 chore: mybatis, mysql connector 디펜던시 추가
    
    * #36 feat: mybatis config 클래스 추가

---
+++ src/main/java/com/fourdays/core/common/config/MyBatisConfig.java
@@ -0,0 +1,35 @@
+package com.fourdays.core.common.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class MyBatisConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource());
+        factoryBean.addMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mybatis/mapper/*-mapper.xml"));
+        return factoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate() throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory());
+    }
+}
