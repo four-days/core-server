35f44f2467f2ddcf03647144965dc8887e4f77b0
김경석 <0xe82de@gmail.com>
2023-07-07 23:59:47 +0900

    #46 refactor: URL, Protocol Repository 스텁 테스트 수정

--- src/test/java/com/fourdays/core/url/application/service/UrlServiceImplTest.java
+++ src/test/java/com/fourdays/core/url/application/service/UrlServiceImplTest.java
@@ -1,7 +1,7 @@
 package com.fourdays.core.url.application.service;
 
-import com.fourdays.core.url.adapter.out.persistence.ProtocolRepositoryMemory;
-import com.fourdays.core.url.adapter.out.persistence.UrlRepositoryMemory;
+import com.fourdays.core.url.adapter.out.persistence.ProtocolRepositoryStub;
+import com.fourdays.core.url.adapter.out.persistence.UrlRepositoryStub;
 import com.fourdays.core.url.application.port.in.UrlService;
 import com.fourdays.core.url.domain.Protocol;
 import com.fourdays.core.url.domain.URL;
@@ -16,7 +16,7 @@ class UrlServiceImplTest {
     @Test
     @DisplayName("url 을 전달하면 url 을 단축해서 urlKey 값을 반환한다.")
     void shortenUrlTest_urlKeyCheck() {
-        UrlService urlService = new UrlServiceImpl(() -> "urlKey", new UrlRepositoryMemory(), new ProtocolServiceImpl(new ProtocolRepositoryMemory()));
+        UrlService urlService = new UrlServiceImpl(() -> "urlKey", new UrlRepositoryStub(), new ProtocolServiceImpl(new ProtocolRepositoryStub()));
         String originalUrl = "https://four.days/";
         String urlKey = urlService.shortenUrl(originalUrl);
 
@@ -26,10 +26,10 @@ class UrlServiceImplTest {
     @Test
     @DisplayName("urlKey 로 원본 url 을 찾을 수 있다.")
     void findOriginalUrlByKeyTest_exists() {
-        UrlRepositoryMemory urlRepository = new UrlRepositoryMemory();
+        UrlRepositoryStub urlRepository = new UrlRepositoryStub();
         Protocol https = new Protocol(1, "HTTPS");
         urlRepository.save(new URL("urlKey", https, "https://four.days/"));
-        UrlServiceImpl urlService = new UrlServiceImpl(new Base62EncoderUsingNanoTime(), urlRepository, new ProtocolServiceImpl(new ProtocolRepositoryMemory()));
+        UrlServiceImpl urlService = new UrlServiceImpl(new Base62EncoderUsingNanoTime(), urlRepository, new ProtocolServiceImpl(new ProtocolRepositoryStub()));
         String originalUrl = urlService.findOriginalUrlByUrlKey("urlKey");
         assertThat(originalUrl).isEqualTo("https://four.days/");
     }
@@ -37,8 +37,8 @@ class UrlServiceImplTest {
     @Test
     @DisplayName("유효하지 않은 urlKey 를 전달하면 null 이 반환된다.")
     void findOriginalUrlByKeyTest_notExists() {
-        UrlRepositoryMemory urlRepository = new UrlRepositoryMemory();
-        UrlServiceImpl urlService = new UrlServiceImpl(new Base62EncoderUsingNanoTime(), urlRepository, new ProtocolServiceImpl(new ProtocolRepositoryMemory()));
+        UrlRepositoryStub urlRepository = new UrlRepositoryStub();
+        UrlServiceImpl urlService = new UrlServiceImpl(new Base62EncoderUsingNanoTime(), urlRepository, new ProtocolServiceImpl(new ProtocolRepositoryStub()));
         String originalUrl = urlService.findOriginalUrlByUrlKey("urlKey");
         assertThat(originalUrl).isNull();
     }
