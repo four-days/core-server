6312f003a65bbc10d591fe8d077a71a05b8f28ef
김경석 <0xe82de@gmail.com>
2023-07-09 00:54:58 +0900

    #54 feat: MyBatis 구현체 추가

---
+++ src/main/java/com/fourdays/core/url/adapter/out/persistence/ProtocolRepositoryMyBatis.java
@@ -0,0 +1,32 @@
+package com.fourdays.core.url.adapter.out.persistence;
+
+import com.fourdays.core.url.adapter.out.persistence.dto.ProtocolDto;
+import com.fourdays.core.url.application.port.out.ProtocolRepository;
+import com.fourdays.core.url.domain.Protocol;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class ProtocolRepositoryMyBatis implements ProtocolRepository {
+
+    private final ProtocolMapper protocolMapper;
+
+    @Override
+    public Protocol save(Protocol protocol) {
+        ProtocolDto protocolDto = ProtocolDto.of(protocol);
+        protocolMapper.save(protocolDto);
+
+        return null;
+    }
+
+    @Override
+    public Optional<Protocol> findByName(String name) {
+        ProtocolDto protocolDto = protocolMapper.findByName(name);
+        if (protocolDto == null) {
+            return Optional.empty();
+        }
+
+        return Optional.of(protocolDto.toEntity());
+    }
+}
