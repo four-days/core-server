e668d0697a8612dc656fb5177a80ade6637a7306
김경석 <0xe82de@gmail.com>
2023-07-12 00:01:08 +0900

    #60 test: setUp() 메서드 추가

--- src/test/java/com/fourdays/core/url/adapter/out/persistence/ProtocolMapperTest.java
+++ src/test/java/com/fourdays/core/url/adapter/out/persistence/ProtocolMapperTest.java
@@ -2,6 +2,7 @@ package com.fourdays.core.url.adapter.out.persistence;
 
 import com.fourdays.core.common.config.MyBatisConfig;
 import com.fourdays.core.url.adapter.out.persistence.dto.ProtocolDto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
@@ -19,32 +20,43 @@ class ProtocolMapperTest {
     @Autowired
     private ProtocolMapper protocolMapper;
 
+    @BeforeEach
+    void setUp() {
+        protocolMapper.removeAll();
+    }
+
     @Test
     @DisplayName("프로토콜을 저장하고, name으로 프토토콜을 조회할 수 있다.")
     void saveTest() {
+        // given
         ProtocolDto protocolDto = ProtocolDto.builder()
-                .name("FTP")
+                .name("HTTP")
                 .build();
         protocolMapper.save(protocolDto);
 
-        ProtocolDto findProtocolDto = protocolMapper.findByName("FTP");
+        // when
+        ProtocolDto findProtocolDto = protocolMapper.findByName("HTTP");
 
+        // then
         assertThat(findProtocolDto.getSeq()).isNotNull();
-        assertThat(findProtocolDto.getName()).isEqualTo("FTP");
+        assertThat(findProtocolDto.getName()).isEqualTo("HTTP");
     }
 
     @Test
     @DisplayName("seq로 프로토콜을 조회할 수 있다.")
     void findBySeqTest() {
+        // given
         ProtocolDto protocolDto = ProtocolDto.builder()
-                .name("FTP")
+                .name("HTTP")
                 .build();
         protocolMapper.save(protocolDto);
-
-        ProtocolDto findProtocolDto = protocolMapper.findByName("FTP");
+        ProtocolDto findProtocolDto = protocolMapper.findByName("HTTP");
         int seq = findProtocolDto.getSeq();
+
+        // when
         ProtocolDto findBySeq = protocolMapper.findBySeq(seq);
 
+        // then
         assertThat(findProtocolDto).isEqualTo(findBySeq);
     }
 }
--- src/test/java/com/fourdays/core/url/adapter/out/persistence/ProtocolRepositoryMyBatisTest.java
+++ src/test/java/com/fourdays/core/url/adapter/out/persistence/ProtocolRepositoryMyBatisTest.java
@@ -4,6 +4,7 @@ import com.fourdays.core.common.config.MyBatisConfig;
 import com.fourdays.core.url.application.port.out.ProtocolRepository;
 import com.fourdays.core.url.config.ProtocolConfig;
 import com.fourdays.core.url.domain.Protocol;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
@@ -21,18 +22,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ProtocolRepositoryMyBatisTest {
 
     @Autowired
+    private ProtocolMapper protocolMapper;
+
+    @Autowired
     private ProtocolRepository protocolRepository;
+
+    @BeforeEach
+    void setUp() {
+        protocolMapper.removeAll();
+    }
 
     @Test
     @DisplayName("프로토콜을 생성하고, 프로토콜명으로 프로토콜을 조회할 수 있다.")
     void saveTest() {
+        // given
         Protocol newProtocol = Protocol.builder()
-                .name("FTP")
+                .name("HTTP")
                 .build();
-
         protocolRepository.save(newProtocol);
-        Optional<Protocol> optionalProtocol = protocolRepository.findByName("FTP");
 
+        // when
+        Optional<Protocol> optionalProtocol = protocolRepository.findByName("HTTP");
+
+        // then
         assertThat(optionalProtocol).isNotEmpty();
         assertThat(optionalProtocol.get().getName()).isEqualTo(newProtocol.getName());
     }
@@ -40,8 +52,10 @@ class ProtocolRepositoryMyBatisTest {
     @Test
     @DisplayName("존재하지 않는 프로토콜명으로 조회하면 빈 Optional이 반환된다.")
     void emptyTest() {
-        Optional<Protocol> optionalProtocol = protocolRepository.findByName("None");
+        // when
+        Optional<Protocol> optionalProtocol = protocolRepository.findByName("HTTP");
 
+        // then
         assertThat(optionalProtocol).isEmpty();
     }
 }
